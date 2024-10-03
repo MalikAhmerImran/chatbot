da881c0d739abb2a932e4393ff7caaaa295d57e7
MalikAhmerImran <bilal.munirr@gmail.com>
2024-10-03 11:47:04 +0500

    question answers and their embeddings are store in mongodb

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ from langchain_groq import ChatGroq
 import os
 import getpass
 from datetime import datetime
+from langchain.embeddings import HuggingFaceEmbeddings
 
-db=MongoClient()
-qa_chatbot=db['qa_chatbot']
+client=MongoClient()
+qa_chatbot=client['qa_chatbot']
 qa_collection=qa_chatbot['qa_collection']
+db=client['embeddings']
+embedding_collection=db['embedding_collection']
 
 llm = ChatGroq(
     model="mixtral-8x7b-32768",
@@ -37,3 +40,18 @@ qa_collection.insert_one(
     }
 
     )
+
+question_answer=[messages[1][1],ai_msg.content]
+
+model_name = "sentence-transformers/all-MiniLM-L6-v2" 
+
+embedding_model = HuggingFaceEmbeddings(model_name=model_name)
+
+embeddings = embedding_model.embed_documents(question_answer)
+
+print(embeddings)
+
+embedding_collection.insert_one({
+        'first_document':embeddings[0],
+        'second_document':embeddings[1]
+    })
